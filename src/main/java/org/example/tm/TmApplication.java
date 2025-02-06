f639e647a767b09fadd5d07f91793c8690892c01
arturdmytriiev <arturdmytriiev@gmail.com>
2025-02-06 16:03:30 +0100

    Write structure and add packages

---
+++ src/main/java/org/example/tm/TmApplication.java
@@ -0,0 +1,12 @@
+package org.example.tm;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmApplication.class, args);
+    }
+
+}
